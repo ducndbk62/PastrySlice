182a4175a304e53f7b2c196da37db3ec10499c3e
Nguyễn Đình Đức <ducndbk62@gmail.com>
2021-08-10 17:51:05 +0700

    +model table, cycle rotate

---
+++ Assets/Scripts/AnimeScripts/CycleRotation1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleRotation1 : MonoBehaviour
+{
+    public Vector3 rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(rotateSpeed);
+    }
+}
